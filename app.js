8af7fe5fc15bf0c4a34d6d161045ce1e5a500f63
Tarasenko Yuriy <y.tarasenko@NB0520.local>
2021-11-05 13:53:06 +0300

    Trying db

--- app.js
+++ app.js
@@ -25,19 +25,41 @@ if (process.env.NODE_ENV === "production") {
 
 const PORT = process.env.PORT || 5000;
 
-const connection = mysql.createConnection({
-  host: dbconfig.host,
-  user: dbconfig.user,
-  password: dbconfig.password,
-  database: dbconfig.database,
-});
+let connection;
+
+const handleDisconnect = () => {
+  connection = mysql.createConnection(dbconfig); // Recreate the connection, since
+  // the old one cannot be reused.
+
+  connection.connect((err) => {
+    // The server is either down
+    if (err) {
+      // or restarting (takes a while sometimes).
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    } else {
+      console.log("Connected!");
+    } // to avoid a hot loop, and to allow our node script to
+  }); // process asynchronous requests in the meantime.
+  // If you're also serving http, display a 503 error.
+  connection.on("error", (err) => {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      // Connection to the MySQL server is usually
+      handleDisconnect(); // lost due to either server restart, or a
+    } else {
+      // connnection idle timeout (the wait_timeout
+      throw err; // server variable configures this)
+    }
+  });
+};
 
 async function start() {
   try {
-    await connection.connect(() => console.log("Connected!"));
     app.listen(PORT, () =>
       console.log(`App has been started on port ${PORT}...`)
     );
+    handleDisconnect();
     db.sequelize.sync();
   } catch (e) {
     console.log("Server Error", e.message);
