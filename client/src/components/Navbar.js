6fd475cb5824a78e77fc5b457e9e279fd319dc3d
Tarasenko Yuriy <y.tarasenko@NB0520.local>
2021-11-05 17:07:11 +0300

    Add sidebar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,7 +2,13 @@ import React, { useContext, useEffect } from "react";
 import { NavLink, useHistory } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import styled from "styled-components";
-import { Dropdown } from "react-materialize";
+import {
+  Button,
+  Dropdown,
+  Icon,
+  SideNav,
+  SideNavItem,
+} from "react-materialize";
 import axios from "axios";
 import { useMessage } from "../hooks/message.hook";
 
@@ -31,87 +37,160 @@ export const Navbar = () => {
 
   return (
     auth.profile && (
-      <NavbarContainer>
-        <div className="nav-wrapper">
-          <span className="brand-logo text-bold black-text">
-            Тестирование SQL
-          </span>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <NavLink to="/" className="black-text waves-effect text-bold">
-                Главная
-              </NavLink>
-              {auth.profile.role === "student" && (
-                <NavLink
-                  to="/test"
-                  className="black-text waves-effect text-bold"
-                >
-                  Начать тест
+      <>
+        <NavbarContainer>
+          <div className="nav-wrapper">
+            <span className="brand-logo text-bold black-text">
+              Тестирование SQL
+            </span>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <NavLink to="/" className="black-text waves-effect text-bold">
+                  Главная
                 </NavLink>
-              )}
-              {auth.profile.role === "teacher" && (
-                <NavLink
-                  to="/create"
-                  className="black-text waves-effect text-bold"
-                >
-                  Создать тест
-                </NavLink>
-              )}
-            </li>
-            <li>
-              <NavLink
-                to="/tables"
-                className="black-text waves-effect text-bold"
-              >
-                Структуры таблиц
-              </NavLink>
-            </li>
-            <li>
-              <Dropdown
-                id="profile"
-                options={{
-                  alignment: "left",
-                  autoTrigger: true,
-                  closeOnClick: true,
-                  constrainWidth: false,
-                  container: null,
-                  coverTrigger: false,
-                  hover: false,
-                  inDuration: 150,
-                  onCloseEnd: null,
-                  onCloseStart: null,
-                  onOpenEnd: null,
-                  onOpenStart: null,
-                  outDuration: 250,
-                }}
-                trigger={
-                  <a
-                    className="black-text waves-effect text-bold dropdown-trigger"
-                    data-target="profile"
+                {auth.profile.role === "student" && (
+                  <NavLink
+                    to="/test"
+                    className="black-text waves-effect text-bold"
                   >
-                    <i className="large material-icons">account_circle</i>
+                    Начать тест
+                  </NavLink>
+                )}
+                {auth.profile.role === "teacher" && (
+                  <NavLink
+                    to="/create"
+                    className="black-text waves-effect text-bold"
+                  >
+                    Создать тест
+                  </NavLink>
+                )}
+              </li>
+              <li>
+                <NavLink
+                  to="/tables"
+                  className="black-text waves-effect text-bold"
+                >
+                  Структуры таблиц
+                </NavLink>
+              </li>
+              <li>
+                <Dropdown
+                  id="profile"
+                  options={{
+                    alignment: "left",
+                    autoTrigger: true,
+                    closeOnClick: true,
+                    constrainWidth: false,
+                    container: null,
+                    coverTrigger: false,
+                    hover: false,
+                    inDuration: 150,
+                    onCloseEnd: null,
+                    onCloseStart: null,
+                    onOpenEnd: null,
+                    onOpenStart: null,
+                    outDuration: 250,
+                  }}
+                  trigger={
+                    <a
+                      className="black-text waves-effect text-bold dropdown-trigger"
+                      data-target="profile"
+                    >
+                      <i className="large material-icons">account_circle</i>
+                    </a>
+                  }
+                >
+                  <span className="black-text">
+                    Fullname: {auth.profile.fullname}
+                  </span>
+                  <span className="black-text">
+                    Email: {auth.profile.email}
+                  </span>
+                  <span className="black-text">
+                    Phone number: {auth.profile.phone_number}
+                  </span>
+                  <span className="black-text">Role: {auth.profile.role}</span>
+                  <span className="black-text">
+                    Group: {auth.profile.study_group}
+                  </span>
+                  <a href="/" onClick={logoutHandler} className="black-text">
+                    <i className="material-icons">exit_to_app</i>Выйти
                   </a>
-                }
-              >
-                <span className="black-text">
-                  Fullname: {auth.profile.fullname}
-                </span>
-                <span className="black-text">Email: {auth.profile.email}</span>
-                <span className="black-text">
-                  Phone number: {auth.profile.phone_number}
-                </span>
-                <span className="black-text">Role: {auth.profile.role}</span>
-                <span className="black-text">
-                  Group: {auth.profile.study_group}
-                </span>
-                <a href="/" onClick={logoutHandler} className="black-text">
-                  <i className="material-icons">exit_to_app</i>Выйти
-                </a>
-              </Dropdown>
-            </li>
-          </ul>
-        </div>
-      </NavbarContainer>
+                </Dropdown>
+              </li>
+            </ul>
+          </div>
+        </NavbarContainer>
+        <SideNav
+          id="navbar"
+          options={{
+            draggable: true,
+          }}
+          trigger={
+            <Button node="button" className="transparent">
+              <Icon>menu</Icon>
+            </Button>
+          }
+        >
+          <SideNavItem
+            user={{
+              background: "https://placeimg.com/640/480/tech",
+              email: auth.profile.email,
+              name: auth.profile.fullname,
+            }}
+            userView
+          />
+          <SideNavItem href="/" className="black-text waves-effect text-bold">
+            Главная
+          </SideNavItem>
+          {auth.profile.role === "student" && (
+            <SideNavItem
+              href="/test"
+              className="black-text waves-effect text-bold"
+            >
+              Начать тест
+            </SideNavItem>
+          )}
+          {auth.profile.role === "teacher" && (
+            <SideNavItem
+              href="/create"
+              className="black-text waves-effect text-bold"
+            >
+              Создать тест
+            </SideNavItem>
+          )}
+          <SideNavItem
+            href="/tables"
+            className="black-text waves-effect text-bold"
+          >
+            Структуры таблиц
+          </SideNavItem>
+          <SideNavItem divider />
+          <SideNavItem
+            href="/"
+            onClick={logoutHandler}
+            className="black-text"
+            waves
+          >
+            <i className="material-icons">exit_to_app</i>Выйти
+          </SideNavItem>
+          <SideNavItem subheader>
+            <span className="black-text">
+              Phone number:{" "}
+              {auth.profile.phone_number !== ""
+                ? auth.profile.phone_number
+                : "--"}
+            </span>
+            <br />
+            <span className="black-text">Role: {auth.profile.role}</span>
+            <br />
+            <span className="black-text">
+              Group: {auth.profile.study_group}
+            </span>
+            <br />
+          </SideNavItem>
+        </SideNav>
+      </>
     )
   );
 };
